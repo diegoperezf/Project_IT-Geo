ca1839e5e0b216ca15ca83bc2d53336dda9d2a82
diegoperezf <diegopforero@gmail.com>
2022-10-25 17:00:10 +0700

    sensor post

--- kabinet/views.py
+++ kabinet/views.py
@@ -13,14 +13,14 @@ def UsersView(request):
 def SensorsView(request):
     sensor_list = Sensor.objects.all()
     return render(request, "kabinet/sensors.html", {"sensor_list" : sensor_list})
-
 @csrf_exempt
 def NewSensor(request):
-    nName = request.POST.get('name')
-    nLon = int(request.POST.get('lon'))
-    nLat = int(request.POST.get('lat'))
-    nSensor = Sensor.objects.create(name=nName, lon=nLon, lat=nLat)
-    return redirect('/kabinet/sensors/')
+    if request.is_ajax() and request.method == 'POST':
+        nName = request.POST.get('name')
+        nLon = int(request.POST.get('lon'))
+        nLat = int(request.POST.get('lat'))
+        Sensor.objects.create(name=nName, lon=nLon, lat=nLat)
+        return redirect('kabinet:sensors')
 
 class DetailView(generic.DetailView):
     model = Sensor
