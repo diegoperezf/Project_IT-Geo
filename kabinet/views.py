c6f5dcd5ede037e2bf70b5ca6212417ecf5ba2bb
diegoperezf <diegopforero@gmail.com>
2022-10-31 23:37:54 +0700

    sensor post_2

--- kabinet/views.py
+++ kabinet/views.py
@@ -5,6 +5,9 @@ from django.views import generic
 from kabinet.models import Sensor
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+from decimal import *
 # Create your views here.
 
 def UsersView(request):
@@ -13,14 +16,18 @@ def UsersView(request):
 def SensorsView(request):
     sensor_list = Sensor.objects.all()
     return render(request, "kabinet/sensors.html", {"sensor_list" : sensor_list})
-@csrf_exempt
+
 def NewSensor(request):
-    if request.is_ajax() and request.method == 'POST':
-        nName = request.POST.get('name')
-        nLon = int(request.POST.get('lon'))
-        nLat = int(request.POST.get('lat'))
+    if request.method == 'POST':
+        body = json.loads(request.body("utf-8"))
+        print(body)
+        nName = body['name'] 
+        nLon = Decimal(body['lon'])
+        nLat = Decimal(body['lat'])
+        print(nName, nLon, nLat)
         Sensor.objects.create(name=nName, lon=nLon, lat=nLat)
-        return redirect('kabinet:sensors')
+        return JsonResponse({'success': 1, 'message': 'Sensor succesfully inserted'})
+        
 
 class DetailView(generic.DetailView):
     model = Sensor
